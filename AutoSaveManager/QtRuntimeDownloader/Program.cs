3b753d0cf501176a2fc96950a07c1700deebe256
TripleWhy <TripleWhy@users.noreply.github.com>
2020-01-21 17:19:15 +0100

    Let CI include Qt and .net into the artifact

---
+++ AutoSaveManager/QtRuntimeDownloader/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace QtRuntimeDownloader
+{
+	using Qml.Net;
+	using Qml.Net.Runtimes;
+	using System;
+	using System.IO;
+	using System.Reflection;
+
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length < 1)
+				return 1;
+
+			RuntimeTarget target = RuntimeTarget.Windows64;
+			MethodInfo runtimeTargetToString = typeof(RuntimeManager).GetMethod("RuntimeTargetToString", BindingFlags.Static | BindingFlags.NonPublic);
+			string targetString = (string)runtimeTargetToString.Invoke(null, new object[] { target });
+			string qtVersion = QmlNetConfig.QtBuildVersion;
+			string qtVersionString = $"{qtVersion}-{targetString}";
+
+			string dstPath = Path.GetFullPath(args[0]);
+			dstPath = Path.Combine(dstPath, qtVersionString);
+
+			Console.WriteLine($"Downloading Qt to {dstPath}...");
+			Directory.CreateDirectory(dstPath);
+			RuntimeManager.DownloadRuntimeToDirectory(qtVersion, target, dstPath);
+			Directory.Exists(dstPath);
+			return File.Exists(Path.Combine(dstPath, "version.txt")) ? 0 : 2;
+		}
+	}
+}
